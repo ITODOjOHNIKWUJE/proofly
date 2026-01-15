dbeaed7b445b237a29d3a3e248a30c7ffadd430e
ITODOjOHNIKWUJE <johnit9@gmail.com>
2026-01-15 17:08:22 +0100

    Remove invalid turbo config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false,
-  },
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+export default nextConfig
