2ba82b353d4b1c400fb5fa0eb284157e1949f20c
ITODOjOHNIKWUJE <johnit9@gmail.com>
2026-01-16 14:32:46 +0100

    Remove Suspense wrapper blocking clicks

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 'use client'
 
-import { Suspense } from 'react'
 import HomeClient from './home-client'
 
 export default function Page() {
-  return (
-    <Suspense fallback={
-  <div className="p-10">
-    <p>Please log in.</p>
-    <a href="/login" className="underline">Go to login</a>
-  </div>
-}>
-      <HomeClient />
-    </Suspense>
-  )
+  return <HomeClient />
 }
