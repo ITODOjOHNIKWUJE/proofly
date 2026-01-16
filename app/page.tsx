79e0db7df25f5f1459c7758dd8a9a9757aaac914
ITODOjOHNIKWUJE <johnit9@gmail.com>
2026-01-16 15:18:23 +0100

    Disable prerendering for root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-export const dynamic = 'force-dynamic'
+'use client'
 
-import HomeClient from './home-client'
+import dynamic from 'next/dynamic'
+
+const HomeClient = dynamic(() => import('./home-client'), {
+  ssr: false,
+})
 
 export default function Page() {
   return <HomeClient />
